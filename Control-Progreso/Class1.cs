5d2277eaa22ceb5bf6c71f7965f316a68182bd15
Patricio Scidá <patricioscida@gmail.com>
2021-06-03 17:02:13 -0300

    Declarados datos en nueva clase

--- Control-Progreso/Class1.cs
+++ Control-Progreso/Class1.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace Control_Progreso {
     class Class1 {
+        private string tipo, nombre, apodo;
+        private int fechaNac, edad, salud;
 
+        enum Caracteristicas {
+            Velocidad = 2, //entre 1 y 10
+            Destreza = 2, // entre 1 y 5
+            Fuerza = 7, // Entre 1 y 10
+            Nivel = 10, // entre 1 y 10
+            Armadura = 4 // entre 1 y 10
+        }
     }
 }
