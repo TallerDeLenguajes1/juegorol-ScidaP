88e89fd6106091f28de83f9a14d6d7e9b36aeb3c
Patricio Scidá <patricioscida@gmail.com>
2021-06-03 17:58:38 -0300

    nuevoPersonaje()

--- Control-Progreso/Class1.cs
+++ Control-Progreso/Class1.cs
@@ -6,15 +6,62 @@ using System.Threading.Tasks;
 
 namespace Control_Progreso {
     class Class1 {
-        private string tipo, nombre, apodo;
-        private int fechaNac, edad, salud;
 
-        enum Caracteristicas {
-            Velocidad = 2, //entre 1 y 10
-            Destreza = 2, // entre 1 y 5
-            Fuerza = 7, // Entre 1 y 10
-            Nivel = 10, // entre 1 y 10
-            Armadura = 4 // entre 1 y 10
+        public enum tipoPersonaje {
+            Elfo,
+            Orco,
+            Hobbit,
+            Humano
+        }
+
+        class Personaje {
+            private tipoPersonaje tipo;
+            private string nombre;
+            private string apodo;
+            private DateTime fechaNac;
+            private int edad;
+            private int salud;
+
+            private int velocidad, destreza, fuerza, nivel, armadura;
+
+            public string Nombre { get => nombre; set => nombre = value; }
+            public string Apodo { get => apodo; set => apodo = value; }
+            public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+            public int Edad { get => edad; set => edad = value; }
+            public int Salud { get => salud; set => salud = value; }
+            public int Velocidad { get => velocidad; set => velocidad = value; }
+            public int Destreza { get => destreza; set => destreza = value; }
+            public int Fuerza { get => fuerza; set => fuerza = value; }
+            public int Nivel { get => nivel; set => nivel = value; }
+            public int Armadura { get => armadura; set => armadura = value; }
+            public tipoPersonaje Tipo { get => tipo; set => tipo = value; }
+
+
+
+            private void nuevoPersonaje() { // no estoy seguro desde dónde llamar a esta funcion
+                Random rand = new Random();
+                Salud = 100;
+                edad = rand.Next(0, 300);
+                Velocidad = rand.Next(1, 10);
+                Destreza = rand.Next(1, 5);
+                Fuerza = rand.Next(1, 10);
+                Nivel = rand.Next(1, 10);
+                Armadura = rand.Next(1, 10);
+                switch (rand.Next(1,5)) {
+                    case 1:
+                        tipo = tipoPersonaje.Elfo;
+                        break;
+                    case 2:
+                        tipo = tipoPersonaje.Hobbit;
+                        break;
+                    case 3:
+                        tipo = tipoPersonaje.Humano;
+                        break;
+                    case 4:
+                        tipo = tipoPersonaje.Orco;
+                        break;
+                }
+            }
         }
     }
 }
